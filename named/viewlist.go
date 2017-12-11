bc1bf64ded5e93e5171f0299503f83780a67bba9
Steven Logue <bitvector2@outlook.com>
2017-12-11 12:08:42 -0800

    updated aced-dns sources

--- named/viewlist.go
+++ named/viewlist.go
@@ -16,24 +16,24 @@ const (
     {{ . }}
     {{- end }}
 view default {
-	zone "." IN {
-		type hint;
-		file "named.ca";
-	};
+    zone "." IN {
+        type hint;
+        file "named.ca";
+    };
 
-	zone "localhost" IN {
-		type master;
-		file "pri/localhost.zone";
-		allow-update { none; };
-		notify no;
-	};
+    zone "localhost" IN {
+        type master;
+        file "pri/localhost.zone";
+        allow-update { none; };
+        notify no;
+    };
 
-	zone "127.in-addr.arpa" IN {
-		type master;
-		file "pri/127.zone";
-		allow-update { none; };
-		notify no;
-	};
+    zone "127.in-addr.arpa" IN {
+        type master;
+        file "pri/127.zone";
+        allow-update { none; };
+        notify no;
+    };
     recursion yes;
 };
 `
